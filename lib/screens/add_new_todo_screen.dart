98ebfb4d67fd100a153473fa51e9aab199a78c66
AshirRahman <md.ashir.rahman@g.bracu.ac.bd>
2025-06-06 23:22:05 +0600

    3 screens

--- lib/screens/add_new_todo_screen.dart
+++ lib/screens/add_new_todo_screen.dart
@@ -10,11 +10,13 @@ class AddNewTodoScreen extends StatefulWidget {
 
 class _AddNewTodoScreenState
     extends State<AddNewTodoScreen> {
-
-  final TextEditingController _titleTEController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
+  final TextEditingController _titleTEController =
+      TextEditingController();
+  final TextEditingController
+  _descriptionController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey =
+      GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -34,15 +36,25 @@ class _AddNewTodoScreenState
               child: Column(
                 children: [
                   TextFormField(
+                    controller: _titleTEController,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
                     decoration: InputDecoration(
                       labelText: 'title',
                       hintText:
                           'write your todo title',
                     ),
-                    validator: ,
+                    validator: (String? value) {
+                      if (value == null ||
+                          value.trim().isEmpty) {
+                        return 'enter you title';
+                      }
+                      return null;
+                    },
                   ),
                   SizedBox(height: 8),
                   TextFormField(
+                    controller: _descriptionController,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
                     minLines: 1,
                     maxLines: 3,
                     decoration: InputDecoration(
@@ -50,13 +62,23 @@ class _AddNewTodoScreenState
                       hintText:
                           'write your todo description',
                     ),
+                    validator: (String? value) {
+                      if (value == null ||
+                          value.trim().isEmpty) {
+                        return 'enter your description';
+                      }
+                      return null;
+                    },
                   ),
                   SizedBox(height: 16),
                   ElevatedButton(
                     onPressed: () {
-                      print(
-                        'clicked submit button',
-                      );
+                      // print(
+                      //   'clicked submit button',
+                      // );
+                      if (_formKey.currentState!.validate()){
+
+                      }
                     },
                     child: Text('submit'),
                   ),
